0acc922ecf95ee85475e5d9b27185387ff7d8048
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-06-21 12:17:44 +0300

    Add changes in index.html and styles.css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
 })
+$(document).ready(function(){
+    $("design1").hide(function)
+    $("picture1").click(function){
+        if (#"design1").is('visile')
+
+    }
+})
